7c86ed34d95348589bb4023a36079221d340e873
DataEngITEX <data.engineer@iisysgroup.com>
2025-09-10 11:04:20 +0100

    made changes to test postgres local connection

--- server/configs/postgres_db.js
+++ server/configs/postgres_db.js
@@ -4,15 +4,15 @@ import pg from "pg";
 const { Pool } = pg;
 import dotenv from "dotenv";
 dotenv.config();
-/**const pool = new Pool({
+const pool = new Pool({
   user: process.env.POSTGRES_DB_USER,
   host: process.env.POSTGRES_DB_HOST,
   database: process.env.POSTGRES_DB_NAME,
   password: process.env.POSTGRES_DB_PASSWORD,
   port: process.env.POSTGRES_DB_PORT,
-}); */
-const pool = new Pool({
+}); 
+/**const pool = new Pool({
   connectionString: process.env.POSTGRES_DATABASE_URL,
   ssl: { rejectUnauthorized: false }, // Required for Render
-});
+});**/
 export default pool;
